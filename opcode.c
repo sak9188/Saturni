31bdd2f96ffda275da5d370e4c821ff864ab86c2
HK <sak9188@163.com>
2021-10-13 11:11:27 +0800

    提交这个语言的范式

--- opcode.c
+++ opcode.c
@@ -4,6 +4,40 @@
 
 #include "opcode.h"
 
+#ifndef MAXSTACK
+#define MAXSTACK 512
+#endif
+
+static Object stack[MAXSTACK] = {{T_MARK, {NULL}}};
+static Object *top=stack+1, *base=stack+1;
+
+static Object* sat_convt2number(Object* obj)
+{
+	static Object cvt;
+
+	if(tag(obj) == T_NUM)
+	{
+		cvt = *obj;
+		return &cvt;
+	}
+
+	tag(&cvt) == T_NONE;
+
+	if(tag(obj) = T_STRING)
+	{
+		char *ptr;
+
+		// 
+		nvalue(&cvt) = strtod(svalue(obj), &ptr);
+		if(*ptr == 0)
+		{
+			tag(&cvt) = T_NUM;
+		}
+	}
+	
+	return &cvt;
+}
+
 // 一些快捷函数
 int sat_is_number(Object* obj)
 {
@@ -53,15 +87,20 @@ void sat_print(void)
 			printf("%g\n", sat_getnum(obj));
 		}else if(sat_is_string(obj))
 		{
-			print("%s\n", sat_getstring(obj));
+			printf("%s\n", sat_getstring(obj));
 		}else if(sat_is_cfunction(obj))
 		{
-			print("cfunction: ")
-		}else if()
+			printf("cfunction: ");
+		}else if(sat_is_userdata(obj))
 		{
-
+			printf("userdata:");
+		}else if(sat_is_table(obj))
+		{
+			printf("table");
+		}else if(sat_is_none(obj))
+		{
+			printf("None\n");
 		}
-		/* code */
 	}
 	
 }
